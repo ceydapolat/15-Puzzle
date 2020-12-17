9eb81700618c8203687714b71ca0efbf37dfa2be
Ceyda_ <humeyraceydapolat@hotmail.com>
2020-12-17 17:15:13 +0300

    First commit

---
+++ 8puzzle/src/Heuristics.java
@@ -0,0 +1,44 @@
+import java.awt.List;
+import java.util.ArrayList;
+
+public class Heuristics {
+	
+	//heuristic class that creates both heuristics
+	
+	public Heuristics() {
+		
+	}
+	
+	//First Heuristic which tells us how many tiles are in an incorrect position
+	
+	public int numCorPos(BoardNode node) {
+		int [][] goal ={{1,2,3,4},{12,13,14,5},{11,0,15,6},{10,9,8,7}};
+		int result = 0;
+		int [][] state = node.getMatrix();
+		for(int i=0; i<state.length; i++) {
+			for(int j=0; j<state.length; j++) {
+				if(goal[i][j]!=state[i][j]) {
+					result += 1;
+				}
+			}
+		}
+		return result;
+	}
+
+	public int manhattan(BoardNode node) {   //second heuristic which uses a goal state to help determined how far argument node tiles are from desired position
+		int [][]goal = {{1,2,3,4},{12,13,14,5},{11,0,15,6},{10,9,8,7}};
+		int result = 0;
+		int [][]state = node.getMatrix();
+		for(int i=0; i<state.length; i++) {
+			for(int j=0; j<state.length; j++) {
+				int value = state[i][j];
+				result += Math.abs(i - node.getRow(value)) + Math.abs(j - node.getCol(value)); 
+			}
+		}
+		return result;
+	}
+	
+	
+	
+
+}
